2c4b582e67de90cc864ef12cf3e3cc317acf433a
Dong <dongn12@uw.edu>
2023-05-06 12:52:00 -0700

    finish login

--- take-action/app/page.tsx
+++ take-action/app/page.tsx
@@ -1,29 +1,29 @@
-import Image from 'next/image'
+import LoginSignup from './components/loginSignup';
+import Link from 'next/link';
 
 let x = "Hello World"
-function testFunction()
-{
+function testFunction() {
   return x
 }
 export default function Home() {
   return (
-<div>
-    <div className="topnav" id="myTopnav">
-      <a href="#home" className="active" >Home</a>
-      <a href="#opportunities">Opportunities</a>
-      <a href="#contact">Contact</a>
-      <a href="#about">About</a>
-      <a href="javascript:void(0);" className="icon" onClick={testFunction()}>
-        <i className="fa fa-bars"></i>
-      </a>
+    <div>
+      <div className="topnav" id="myTopnav">
+        <a href="/" className="active" >Home</a>
+        <a href="#opportunities">Opportunities</a>
+        <Link href="/contact">Contact</Link>
+        <a href="#about">About</a>
+        <a href="javascript:void(0);" className="icon">
+          <i className="fa fa-bars"></i>
+        </a>
+      </div>
+      <LoginSignup />
+      <main>
+      </main>
+
+      <footer>
+
+      </footer>
     </div>
-
-    <main> 
-    </main>
-
-    <footer> 
-        
-    </footer>
-</div>
   )
 }
