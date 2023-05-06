a66f6e92dc6cf93018121961587b5430824d0eb5
Tim <82545321+DonotDonut@users.noreply.github.com>
2023-05-06 11:07:03 -0700

    putting it back

--- take-action/app/page.tsx
+++ take-action/app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
   return (
 <div> 
     <div className="topnav" id="myTopnav">
-      <a href="page.tsx" className="active" >Home</a>
+      <a href="#home" className="active" >Home</a>
       <a href="#opportunities">Opportunities</a>
-      <a href="Contact.tsx">Contact</a>
+      <a href="Contac.tsx">Contact</a>
       <a href="#about">About</a>
       <a href="javascript:void(0);" className="icon" onClick={testFunction()}>
         <i className="fa fa-bars"></i>
