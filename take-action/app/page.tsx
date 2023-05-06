38fa47fbc8a5476d91785cf107df47cfaff475a5
Dong <dongn12@uw.edu>
2023-05-06 14:34:27 -0700

    test commit

--- take-action/app/page.tsx
+++ take-action/app/page.tsx
@@ -1,40 +1,29 @@
-
-import LoginSignup from './components/loginSignup';
-import Link from 'next/link';
-
+import logo from './img/logo.png'
+import sponsor1 from './img/vwlogofull.svg'
+import sponsor2 from './img/logo_black.png'
+import sponsor3 from './img/th.jpg'
 import Image from 'next/image'
+import './homePage.css'
 
-
-
-let x = "Hello World"
-function testFunction() {
-  return x
-}
 export default function Home() {
   return (
 
-    <div>
-      <div className="topnav" id="myTopnav">
-        <a href="/" className="active" >Home</a>
-        <a href="#opportunities">Opportunities</a>
-        <Link href="/contact">Contact</Link>
-        <a href="#about">About</a>
-        <a href="javascript:void(0);" className="icon">
-          <i className="fa fa-bars"></i>
-        </a>
+    <div className='homePage py-5 mx-auto'>
+      <header className='homePage__header flex text-lg items-center'>
+        <div className='homePage__header__content'>
+          <h1 className='text-5xl my-10'>TakeAction</h1>
+          <p>TakeAction is platform help connect volunteers with activities around neighborhood</p>
+        </div>
+        <Image src={logo} alt="logo" />
+      </header>
+      <div className="HomePage__partners mt-10">
+        <h2 className='text-3xl'>Our partner</h2>
+        <div className="homePage__partners flex px-5 py-10 justify-evenly mt-5">
+          <Image src={sponsor2} alt="" />
+          <Image src={sponsor3} alt="" />
+        </div>
       </div>
-      <LoginSignup />
-      <main>
-      </main>
-
-      <footer>
-
-      </footer>
     </div>
-
-
-
-
 
   )
 }
