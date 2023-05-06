8d4fc13b1f3af4c44e0bfe7f61f261e29fc6a2a1
Dong <dongn12@uw.edu>
2023-05-06 14:30:27 -0700

    design the home page

---
+++ take-action/app/components/navBar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+export default function navBar() {
+    return (
+        <div className="topnav" id="myTopnav">
+            <Link href="/" className="active" >Home</Link>
+            <Link href="/opportunities">Opportunities</Link>
+            <Link href="/contact">Contact</Link>
+            <Link href="/about">About</Link>
+            <Link href="javascript:void(0);" className="icon">
+                <i className="fa fa-bars"></i>
+            </Link>
+        </div>
+    )
+}
--- take-action/app/contact/page.tsx
+++ take-action/app/contact/page.tsx
@@ -7,22 +7,10 @@ function testFunction() {
 export default function contact() {
     return (
         <div>
-            <div className="topnav" id="myTopnav">
-                <a href="#home" className="active" >Home</a>
-                <a href="#opportunities">Opportunities</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
-                <a href="javascript:void(0);" className="icon">
-                    <i className="fa fa-bars"></i>
-                </a>
-            </div>
 
-            <main>
-            </main>
+
+
             <h1>Contact page</h1>
-            <footer>
-
-            </footer>
         </div>
     )
 }
