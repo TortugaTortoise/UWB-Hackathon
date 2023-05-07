fd0154e91ee28f8816081f253af9eb621b46257b
[Home] <[ffgssde15224@gmail.com]>
2023-05-07 03:52:12 -0700

    i love php

---
+++ take-action/app/login/Config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'catisnameofcat.tk');
+   define('DB_USERNAME', 'admin');
+   define('DB_PASSWORD', '123456789');
+   define('DB_DATABASE', 'wpdb');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
