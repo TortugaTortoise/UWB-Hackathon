e4118faeab1cdea6f0fe71c24f48eaf11a6e2863
[Home] <[ffgssde15224@gmail.com]>
2023-05-07 00:30:27 -0700

    turn of the login site

--- take-action/app/components/loginSignup.tsx
+++ take-action/app/components/loginSignup.tsx
@@ -16,67 +16,67 @@ export default function loginSignup() {
         setIsCloseForm(!isCloseForm)
     }
 
-    const handleOnChangeEmail = (e) => {
-        setUserEmail(e.target.value)
-    }
-    const handleOnChangePassword = (e) => {
-        console.log(e.target.value)
-        setUserPassword(e.target.value)
+    // const handleOnChangeEmail = (e) => {
+    //     setUserEmail(e.target.value)
+    // }
+    // const handleOnChangePassword = (e) => {
+    //     console.log(e.target.value)
+    //     setUserPassword(e.target.value)
 
-    }
-    const handleLogin = async () => {
-        console.log(userEmail, userPassword)
-        const token = await fetch('//// server url', {
-            headers: {
-                email: userEmail,
-                password: userPassword
-            }
-        })
-        if (token == 'Error') {
-            setIsUserPasswordEmailValid(false)
-        }
-        else {
-            isUserPasswordEmailValid(true)
-        }
-    }
+    // }
+    // const handleLogin = async () => {
+    //     console.log(userEmail, userPassword)
+    //     const token = await fetch('//// server url', {
+    //         headers: {
+    //             email: userEmail,
+    //             password: userPassword
+    //         }
+    //     })
+    //     if (token == 'Error') {
+    //         setIsUserPasswordEmailValid(false)
+    //     }
+    //     else {
+    //         isUserPasswordEmailValid(true)
+    //     }
+    // }
 
-    return (
-        <div style={closeFormStyle}>
-            {/**modal */}
-            <div className="modal" onClick={handleModal}></div>
-            {/*form */}
-            <div className="loginSignupForm py-10 px-6">
-                {/*transition buttons */}
-                <div className='loginSignupForm__buttons flex '>
-                    <button className='login__button'>Login</button>
-                    <button className='signup__button'>Sign up</button>
-                </div>
-                {/*login form */}
-                <div className='flex flex-col login__forms '>
-                    {!isUserPasswordEmailValid && <p>Invalid email or password</p>}
-                    <input
-                        type='text'
-                        placeholder='email'
-                        className='my-2 p-1 text-lg'
-                        value={userEmail}
-                        onChange={handleOnChangeEmail}
-                    />
-                    <input
-                        type='text'
-                        placeholder='password'
-                        className='my-2 p-1 text-lg'
-                        value={userPassword}
-                        onChange={handleOnChangePassword}
-                    />
-                    <p className='my-5'>forgot password</p>
-                    <button
-                        className='py-2'
-                        onClick={handleLogin}
-                    >Login</button>
-                </div>
+    // return (
+    //     <div style={closeFormStyle}>
+    //         {/**modal */}
+    //         <div className="modal" onClick={handleModal}></div>
+    //         {/*form */}
+    //         <div className="loginSignupForm py-10 px-6">
+    //             {/*transition buttons */}
+    //             <div className='loginSignupForm__buttons flex '>
+    //                 <button className='login__button'>Login</button>
+    //                 <button className='signup__button'>Sign up</button>
+    //             </div>
+    //             {/*login form */}
+    //             <div className='flex flex-col login__forms '>
+    //                 {!isUserPasswordEmailValid && <p>Invalid email or password</p>}
+    //                 <input
+    //                     type='text'
+    //                     placeholder='email'
+    //                     className='my-2 p-1 text-lg'
+    //                     value={userEmail}
+    //                     onChange={handleOnChangeEmail}
+    //                 />
+    //                 <input
+    //                     type='text'
+    //                     placeholder='password'
+    //                     className='my-2 p-1 text-lg'
+    //                     value={userPassword}
+    //                     onChange={handleOnChangePassword}
+    //                 />
+    //                 <p className='my-5'>forgot password</p>
+    //                 <button
+    //                     className='py-2'
+    //                     onClick={handleLogin}
+    //                 >Login</button>
+    //             </div>
 
-                {/*signup form */}
-            </div>
-        </div>
-    )
+    //             {/*signup form */}
+    //         </div>
+    //     </div>
+    // )
 }
